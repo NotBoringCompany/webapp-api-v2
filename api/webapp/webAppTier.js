f342da76e44a5bdcda278d933315a295fe70eaac
suwandre <suwandresukijat@gmail.com>
2022-10-10 21:44:05 +0200

    implemented marketplace.js

---
+++ api/webapp/webAppTier.js
@@ -0,0 +1,16 @@
+/**
+ * `addMonthlyTradingVolume` adds `amount` to `address`'s monthly trading volume.
+ * This is called after a successful sale/purchase of items in our web app marketplace.
+ * Note: This ONLY supports our web app. Any NFT purchases or sales in OpenSea are NOT included.
+ * @param {String} address the address of the user
+ * @param {Number} amount the dollar value of the amount to be increased.
+ * @return {Object} an object with 'status: OK' if successful, or an error thrown otherwise.
+ */
+const addMonthlyTradingVolume = async (address, amount) => {
+    try {
+
+    } catch (err) {
+        throw err;
+    }
+
+};
