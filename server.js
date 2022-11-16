5c0dc1d2b909f47560d3bec6d0fc08be09cfe493
suwandre <suwandresukijat@gmail.com>
2022-11-16 13:02:47 +0100

    added genesis nbmon route to server.js

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const masterKey = process.env.MORALIS_MASTERKEY;
 app.use(cors());
 app.use(express.json());
 
+const genesisNBMon = require('./routes/genesisNBMon');
+
+app.use('/genesisNBMon', genesisNBMon);
+
 app.listen(port, async () => {
     console.log(`Listening from port ${port}`);
 
